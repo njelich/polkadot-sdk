eed93c725381e6fb7c555a0c9ae09e3b83077c4c
Branislav Kontur <bkontur@gmail.com>
2024-04-16 13:00:13 +0200

    Revert `HandleHrmpChannelAccepted` from `pallet_xcm`

--- cumulus/parachains/runtimes/assets/asset-hub-westend/src/xcm_config.rs
+++ cumulus/parachains/runtimes/assets/asset-hub-westend/src/xcm_config.rs
@@ -660,7 +660,7 @@ impl xcm_executor::Config for XcmConfig {
 	type Aliasers = Nothing;
 	type TransactionalProcessor = FrameTransactionalProcessor;
 	type HrmpNewChannelOpenRequestHandler = ();
-	type HrmpChannelAcceptedHandler = (PolkadotXcm,);
+	type HrmpChannelAcceptedHandler = ();
 	type HrmpChannelClosingHandler = ();
 }
 
--- cumulus/parachains/runtimes/collectives/collectives-westend/src/xcm_config.rs
+++ cumulus/parachains/runtimes/collectives/collectives-westend/src/xcm_config.rs
@@ -295,7 +295,7 @@ impl xcm_executor::Config for XcmConfig {
 	type Aliasers = Nothing;
 	type TransactionalProcessor = FrameTransactionalProcessor;
 	type HrmpNewChannelOpenRequestHandler = ();
-	type HrmpChannelAcceptedHandler = (PolkadotXcm,);
+	type HrmpChannelAcceptedHandler = ();
 	type HrmpChannelClosingHandler = ();
 }
 
